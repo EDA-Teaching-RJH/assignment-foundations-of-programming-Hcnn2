e73bd34f5e18021d7014b8ed2d1b97d95977c0a7
Hcnn2 <hcnn2@kent.ac.uk>
2026-02-15 19:23:13 +0000

    database

---
+++ fleet_manager.py
@@ -0,0 +1,6 @@
+def init_database():
+    names = ["Jean-Luc Picard", "William T. Riker", "Geordi La Forge", "Data", "Beverly Crusher"]
+    ranks = ["Captain", "Commander", "Lieutenant Commander", "Lieutenant Commander", "Commander"]
+    divs  = ["Command", "Command", "Operations", "Operations", "Sciences"]
+    ids   = ["PIC1", "RIK2", "LAF3", "DAT4", "CRU5"]
+    return names, ranks, divs, ids
