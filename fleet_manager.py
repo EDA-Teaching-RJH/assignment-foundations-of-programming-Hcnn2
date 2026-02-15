34a530665688849b825e1a442e8287aea63e058e
Hcnn2 <hcnn2@kent.ac.uk>
2026-02-15 19:29:25 +0000

    displaymenu

--- fleet_manager.py
+++ fleet_manager.py
@@ -4,3 +4,14 @@ def init_database():
     divs  = ["Command", "Command", "Operations", "Operations", "Sciences"]
     ids   = ["PIC1", "RIK2", "LAF3", "DAT4", "CRU5"]
     return names, ranks, divs, ids
+
+def display_menu(current_user):
+    print("STARFLEET MANAGER")
+    print(f"Logged in as: {current_user}")
+    print("\n1. Add New Crew Member")
+    print("2. Remove Crew Member")
+    print("3. Update Rank")
+    print("4. Display Full Roster")
+    print("5. Count Senior Officers")
+    print("6. Exit")
+    return input("Enter your choice (1-5): ")
